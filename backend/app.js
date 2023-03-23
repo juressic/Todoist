22b569281b98552707d9d0b0e653b0f394a757bf
virtualant <juremiskov@gmail.com>
2023-03-23 19:21:03 +0100

    cors options

--- backend/app.js
+++ backend/app.js
@@ -15,16 +15,16 @@ app.listen(port, () => {
   console.log(`server started at port ${port}`);
 });
 
-/*const corsOptions = {
-  origin: 'http://localhost:3000',
+const corsOptions = {
+  origin: 'http://localhost:5555',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions));*/
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'build')));
 //MIDDLEWARES
-app.use(cors());
+//app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(TodoRoute);
