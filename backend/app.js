7daf52e6974432dd7fd6eddd89de2400559a2223
virtualant <juremiskov@gmail.com>
2023-03-25 11:10:45 +0100

    public folder address changed

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(AuthRoute);
 app.use(UsersRoute);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 //ERROR HANDLER
