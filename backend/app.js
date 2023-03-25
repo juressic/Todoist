3ba84e509a6a9429b0fd721dc8ed203d3909c2f0
virtualant <juremiskov@gmail.com>
2023-03-25 11:51:57 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 //MIDDLEWARES
